c542fedf86286db002fb0a8c04f98d885b6b9202
sa-co-ta <95979890+sa-co-ta@users.noreply.github.com>
2022-01-29 11:08:55 +0000

    changes img imports case deletes favicon and logos from public folder deletes manifest.json

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,26 +1,26 @@
 import React from "react";
-import TwitterIcon from "../images/twitter-icon.svg";
-import GithubIcon from "../images/github-icon.svg";
-import MirrorIcon from "../images/mirror-icon.svg";
+import twittericon from "../images/twitter-icon.svg";
+import githubicon from "../images/github-icon.svg";
+import mirroricon from "../images/mirror-icon.svg";
 
 export default function Social() {
   return (
     <aside className="social">
       <a href="https://twitter.com/sacota_creative" target="_blank">
         <img
-          src={TwitterIcon}
+          src={twittericon}
           alt="twitter logo linking to Sacota.eth's twitter profile"
         />
       </a>
       <a href="https://github.com/sa-co-ta" target="_blank">
         <img
-          src={GithubIcon}
+          src={githubicon}
           alt="github logo linking to Sacota.eth's github repository"
         />
       </a>
       <a href="https://mirror.xyz/sacota.eth" target="_blank">
         <img
-          src={MirrorIcon}
+          src={mirroricon}
           alt="mirror logo linking to Sacota.eth's blog on mirror.xyz"
         />
       </a>
